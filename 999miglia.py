150ad2eb3bdf9f3bf1ef4f036463fb4c8eb7c84d
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2016-12-23 15:46:52 +0100

    cleanup

--- 999miglia.py
+++ 999miglia.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-tmpdir='segdir'
-
-import os, shutil, sys, zipfile, tempfile
-
 import urllib3
-import io
 
 import gpxpy
 import gpxpy.gpx
@@ -45,7 +40,6 @@ class Part:
         self.segmentId = segmentId
         request = self.http.request('GET', "http://export.openrunner.com/kml/exportImportGPX.php?rttype=0&id={}".format(segmentId))
         data = request.data.decode()
-        f = io.StringIO(data)
         self.gpx = gpxpy.parse(data)
         assert len(self.gpx.tracks) == 1, 'Openrunner ID {} has {} tracks'.format(segmentId, len(self.gpx.tracks))
         self.track = self.gpx.tracks[0]
@@ -71,38 +65,39 @@ class Part:
         for src, dest in zip(waypointOrder, Part.allParts):
             dest.endWaypoint = src.startWaypoint
         for idx, part in enumerate(Part.allParts):
-            part.endWaypoint.name ='CP {}: {}'.format(idx+1, part.endWaypoint.name)
-            part.endWaypoint.description ='Distance {}km uphill {}m\nClosing time (to be done)'.format(part.length, part.uphill)
+            part.endWaypoint.name = 'CP {}: {}'.format(idx+1, part.endWaypoint.name)
+            part.endWaypoint.description = 'Distance {}km uphill {}m\nClosing time (to be done)'.format(part.length, part.uphill)
 
     @staticmethod
     def parseParts(segmentIds):
-        Part.allParts = list(Part(x) for x in segmentIds) 
+        Part.allParts = list(Part(x) for x in segmentIds)
         Part.setEndWaypoints()
 
-Part.parseParts([6875583, 6875588, 6875600, 6875608, 6875873, 6875883, 6875889, 6875894, 6876542, 6875912, 6875939, 6876273, 6876301, 6876446, 6876412])
+    @staticmethod
+    def createFullTrackFor(segmentIds, intoFileName):
+        Part.parseParts(segmentIds)
+        fullGpx = gpxpy.gpx.GPX()
+        fullGpx.waypoints = list(x.endWaypoint for x in Part.allParts)
+        segment = gpxpy.gpx.GPXTrackSegment()
+        allPoints = sum((x.points for x in Part.allParts), list())
+        for point in allPoints:
+            point.remove_time()
+        segment.points = allPoints
+
+        fullLength = sum(x.length for x in Part.allParts)
+        fullUphill = sum(x.uphill for x in Part.allParts)
+
+        print('the full track has {} points, {}km and {}m'.format(len(segment.points), fullLength, fullUphill))
 
 
+        track = gpxpy.gpx.GPXTrack()
+        track.segments.append(segment)
 
-fullGpx = gpxpy.gpx.GPX()
-fullGpx.waypoints = list(x.endWaypoint for x in Part.allParts)
+        fullGpx.tracks.append(track)
 
-segment = gpxpy.gpx.GPXTrackSegment()
+        with open(intoFileName, 'w') as fullFile:
+            fullFile.write(fullGpx.to_xml())
 
-allPoints = sum((x.points for x in Part.allParts), list())
-for point in allPoints:
-    point.remove_time()
-segment.points =  allPoints
+SEGMENTIDS = (6875583, 6875588, 6875600, 6875608, 6875873, 6875883, 6875889, 6875894, 6876542, 6875912, 6875939, 6876273, 6876301, 6876446, 6876412)
 
-fullLength = sum(x.length for x in Part.allParts)
-fullUphill = sum(x.uphill for x in Part.allParts)
-
-print('the full track has {} points, {}km and {}m'.format(len(segment.points), fullLength, fullUphill))
-
-
-track = gpxpy.gpx.GPXTrack()
-track.segments.append(segment)
-
-fullGpx.tracks.append(track)
-
-with open('999.gpx', 'w') as fullFile:
-    fullFile.write(fullGpx.to_xml())
+Part.createFullTrackFor(SEGMENTIDS, '999.gpx')
